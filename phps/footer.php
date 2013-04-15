ee5b9d26f0176226e5b8e6932bb5f3802fb33500
flrngel <flrngel@gmail.com>
2013-04-15 15:19:18 +0900

    footer change

--- phps/footer.php
+++ phps/footer.php
@@ -1,17 +1,26 @@
 
-            <!--require end-->
+			<!--require end-->
 
-            <hr>
+			<hr/>
 
-            <footer>
-				<p>&copy; lavida.us 2008-2013 <a href="mailto:flrngel@gmail.com">Hwe-chul Cho</a>, <a href="mailto:libe.ajou.ac.kr">Hyun-hwan Jeong</a></p>
-            </footer>
+			<footer>
+				<div class="row-fluid">
+					<center>
+						<a href="https://github.com/flrngel/LavidaOnlineJudge.git" target="_blank"><img src="/image/github.png"></a>
+					</center>
+				</div>
+				<div class="row-fluid" style="margin-top:10px">
+					<center>
+						<p>&copy; lavida.us 2008-2013 <a href="mailto:flrngel@gmail.com">Hwe-chul Cho</a>, <a href="mailto:libe.ajou.ac.kr">Hyun-hwan Jeong</a></p>
+					</center>
+				</div>
+			</footer>
 
-        </div> <!-- /container -->
+		</div> <!-- /container -->
 
-        <!-- Le javascript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faste -->
-        <script src="/js/bootstrap.min.js"></script>
-    </body>
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faste -->
+		<script src="/js/bootstrap.min.js"></script>
+	</body>
 </html>
