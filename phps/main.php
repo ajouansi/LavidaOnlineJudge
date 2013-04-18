7965625659728bb8f9422cd220be20904621ce45
flrngel <flrngel@gmail.com>
2013-04-18 19:30:03 +0900

    news position fix

--- phps/main.php
+++ phps/main.php
@@ -7,7 +7,7 @@
 			<h1>Lavida Online Judge</h1><br/>
 			<p><a href="https://www.google.com/intl/ko/chrome/browser/?hl=ko&brand=CHMI" target="_blank">구글 크롬 브라우저</a>와 <a href="http://hangeul.naver.com/font" target="_blank">나눔글꼴</a>을 이용하시면 더욱 더 최적화된 화면을 보실 수 있습니다!</p>
 			<p>This site is optimized for <a href="https://www.google.com/intl/ko/chrome/browser/?hl=ko&brand=CHMI" target="_blank">Google Chrome Browser</a> and <a href="http://hangeul.naver.com/font" target="_blank">Nanum Font</a>!
-			<p class="pull-right"><a href="/news" class="btn btn-info">News</a></p>
+			<p style="margin-top:30px;"><a href="/news" class="btn btn-info">News</a></p>
 		</div>
 	</div>
 </div>
