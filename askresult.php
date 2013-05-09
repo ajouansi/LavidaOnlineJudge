7499a2f33005cfc7b4b8a7cdbaf0d696903f9890
flrngel <flrngel@gmail.com>
2013-05-09 10:17:35 +0900

    fixed global variable problem

--- askresult.php
+++ askresult.php
@@ -1,6 +1,7 @@
 <?
 	session_start();
 	require_once('include/db_info.inc.php');
+	$rid=$_POST['rid'];
 	$sql="SELECT `result` from solution where solution_id='$rid'";
 	$tmp=@mysql_query($sql);
 	$res=mysql_fetch_object($tmp);
