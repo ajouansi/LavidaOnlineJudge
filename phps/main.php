5cd4ccb364a839b51cd9f3dd9b9e50b7ece191e2
flrngel <flrngel@gmail.com>
2013-05-27 18:27:24 +0900

    fork me on github

--- phps/main.php
+++ phps/main.php
@@ -3,10 +3,11 @@
 ?>
 <div class="row">
 	<div class="span12">
-		<div class="hero-unit">
+		<div class="hero-unit" style="position:relative">
 			<h1>Lavida Online Judge</h1><br/>
 			<p><a href="https://www.google.com/intl/ko/chrome/browser/?hl=ko&brand=CHMI" target="_blank">구글 크롬 브라우저</a>와 <a href="http://hangeul.naver.com/font" target="_blank">나눔글꼴</a>을 이용하시면 더욱 더 최적화된 화면을 보실 수 있습니다!</p>
 			<p>This site is optimized for <a href="https://www.google.com/intl/ko/chrome/browser/?hl=ko&brand=CHMI" target="_blank">Google Chrome Browser</a> and <a href="http://hangeul.naver.com/font" target="_blank">Nanum Font</a>!
+			<p><a target="_blank" href="https://github.com/flrngel/LavidaOnlineJudge.git"><img style="position: absolute; top: 0; right: 0; border: 0;" src="/image/forkme.png" alt="Fork me on GitHub"></a></p>
 			<p style="margin-top:30px;"><a href="/news" class="btn btn-info">News</a></p>
 		</div>
 	</div>
