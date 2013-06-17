d447f6324ef72adad9c34a891a7484d1995c9720
flrngel <flrngel@gmail.com>
2013-06-17 16:44:55 +0900

    magic_recursive patch

--- magic.php
+++ magic.php
@@ -1,4 +1,11 @@
 <?
-	$_GET=array_map("addslashes",$_GET);
-	$_POST=array_map("addslashes",$_POST);
+	function array_map_r($callback,$var){
+		$rarr=Array();
+		foreach( $var as $key => $value ){
+			$rarr[$key]=is_array($value)?array_map_r($callback,$value):$callback($value);
+		}
+		return $rarr;
+	}
+	$_GET=array_map_r("addslashes",$_GET);
+	$_POST=array_map_r("addslashes",$_POST);
 ?>
