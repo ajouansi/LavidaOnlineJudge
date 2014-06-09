22af50ede11b6f6a3479dee362956b8f610598d4
flrngel <flrngel@gmail.com>
2014-06-09 16:54:28 +0900

    stablizing

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,9 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/trusty64"
-	config.vm.name = "LOJ VM"
+
+	config.vm.define :LOJVM do |vm|
+	end
 
 	config.vm.provision "shell" do |shell|
 		shell.path="ops/scripts/init.sh"
--- ops/scripts/init.sh
+++ ops/scripts/init.sh
@@ -3,6 +3,9 @@
 
 # settings
 
+mkdir /lavida
+export -x HOME=/lavida
+
 mkdir ~/setup
 mkdir ~/setup/src
 
@@ -10,7 +13,7 @@ mkdir ~/package
 mkdir ~/package/apache2
 
 # install components
-sudo apt-get install -y git lxc curl libcurl3 libcurl-dev wget unzip tree rdate
+sudo apt-get install -y git lxc curl libcurl3 libcurl3-dev wget unzip tree rdate build-essential
 
 # time setting
 sudo rdate -s time.bora.net
@@ -21,17 +24,17 @@ sudo rdate -s time.bora.net
 # install apache
 
 ## apr
-wget http://apache.tt.co.kr//apr/apr-1.5.1.tar.gz -O ~/setup/src/apr-1.5.1.tar.gz
+wget -quiet http://apache.tt.co.kr//apr/apr-1.5.1.tar.gz -O ~/setup/src/apr-1.5.1.tar.gz
 cd ~/setup/src/
 tar xvzf apr-1.5.1.tar.gz
 
 ## apr-util
-wget http://apache.tt.co.kr//apr/apr-util-1.5.3.tar.gz -O ~/setup/src/apr-util-1.5.3.tar.gz
+wget -quiet http://apache.tt.co.kr//apr/apr-util-1.5.3.tar.gz -O ~/setup/src/apr-util-1.5.3.tar.gz
 cd ~/setup/src/
 tar xvzf apr-util-1.5.3.tar.gz
 
 ## pcre
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.35.tar.gz
+wget -quiet ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.35.tar.gz
 cd ~/setup/src/
 tar xvzf pcre-8.35.tar.gz
 cd pcre-8.35
@@ -42,17 +45,17 @@ make && sudo make install
 sudo ldconfig
 
 ## apache2
-wget http://mirror.apache-kr.org//httpd/httpd-2.4.9.tar.gz -O ~/setup/src/apache-2.4.9.tar.gz
+wget -quiet http://mirror.apache-kr.org//httpd/httpd-2.4.9.tar.gz -O ~/setup/src/apache-2.4.9.tar.gz
 cd ~/setup/src/
 tar xvzf apache-2.4.9.tar.gz
-cd apache-2.4.9
+cd httpd-2.4.9
 mv ~/setup/src/apr-1.5.1 ./srclib/apr
 mv ~/setup/src/apr-util-1.5.3 ./srclib/apr-util
 ./configure --prefix=~/package/apache2 --with-included-apr
 make && make install
 
 # clone lavida
-git clone https://github.com/flrngel/lavida ~/lavida-webserver
+git clone https://github.com/flrngel/LavidaOnlineJudge ~/web
 
 ## apache2 config
 sudo rm -rf ~/package/apache2/htdocs
@@ -65,7 +68,7 @@ sudo cp ~/package/apache2/bin/apachectl /etc/init.d/apache2
 
 
 # install mysql
-wget http://ftp.kaist.ac.kr/mariadb/mariadb-10.0.11/source/mariadb-10.0.11.tar.gz -O ~/setup/src/mariadb-10.0.11.tar.gz
+wget -quiet http://ftp.yz.yamagata-u.ac.jp/pub/dbms/mariadb/mariadb-10.0.11/source/mariadb-10.0.11.tar.gz -O ~/setup/src/mariadb-10.0.11.tar.gz
 cd ~/setup/src
 tar xvzf mariadb-10.0.11.tar.gz
 cd mariadb-10.0.11
