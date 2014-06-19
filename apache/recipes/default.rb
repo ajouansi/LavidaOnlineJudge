1866da445cf1d1756a9c63b3329c59e2a65e69c9
flrngel <flrngel@gmail.com>
2014-06-19 13:03:25 +0900

    user typo

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -61,6 +61,6 @@ bash "httpd_install" do
 
 		sudo cp -rf #{node['loj']['path']}/packages/apache2/bin/apachectl /etc/init.d/apache2
 
-		echo "PATH=\$PATH:#{node['loj']['path']}/packages/apache2/bin" >> ~#{node['loj']['path']}/.bashrc
+		echo "PATH=\$PATH:#{node['loj']['path']}/packages/apache2/bin" >> ~#{node['loj']['user']}/.bashrc
 	EOH
 end
