23c125c6782f2e9bdd791a7db86c331a2412cc2c
Cho Hwechul <flrngel@gmail.com>
2014-07-20 17:06:14 +0900

    Update default.rb

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -61,8 +61,6 @@ bash "httpd_install" do
 		ln -s #{node['loj']['path']}/web #{node['loj']['path']}/packages/apache2/htdocs
 
 		sudo cp -rf #{node['loj']['path']}/packages/apache2/bin/apachectl /etc/init.d/apache2
-
-		echo \"PATH=$PATH:#{node['loj']['path']}/packages/apache2/bin\" >> ~#{node['loj']['user']}/.bashrc
 	EOH
 end
 
